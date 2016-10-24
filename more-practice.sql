50f4674034a1fbd72c379676ad2e3122e88a934e
Franziska von der Goltz <franziska@vdgoltz.eu>
2016-10-24 03:06:46 +0000

    completed more practice section

--- more-practice.sql
+++ more-practice.sql
@@ -4,8 +4,25 @@
 
 -- INSERT
 
+INSERT INTO models (year, brand_name, name) VALUES (2015, 'Chevrolet', 'Malibu');
+
+INSERT INTO models (year, brand_name, name) VALUES (2015, 'Subaru', 'Outback');
 
 -- CREATE TABLE
 
+CREATE TABLE awards(
+    ID SERIAL PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    year INTEGER,
+    winner_id INTEGER NOT NULL,
+);
+
+
 
 -- More INSERT
+
+
+INSERT INTO awards (name, year, winner_id) VALUES ('IIHS Safety Award Year', 2015, 49);
+
+INSERT INTO awards (name, year, winner_id) VALUES ('IIHS Safety Award Year', 2015, 50);
+
